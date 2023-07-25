b88f19167eeab474369c0a3940deb039caca143b
james-joobs <jgh3021@naver.com>
2023-07-25 12:54:47 +0900

    adding python jupyter files

---
+++ python/my_module.py
@@ -0,0 +1,10 @@
+def say_hello(name):
+    print(name + "님, 안녕하세요!")
+    
+def get_fullname(last_name, first_name):
+    full_name = last_name + " " + first_name
+    return full_name
+
+def add_numbers(a, b):
+    sum = a+b
+    return sum
